324e2e3ce5740277a507162335df901e770336d4
anurieroglu <ahmetnurieroglu@gmail.com>
2023-09-07 18:24:02 +0300

    cpp1 ex04 done

---
+++ cpp_1/ex04/main.cpp
@@ -0,0 +1,31 @@
+#include <fstream>
+#include <iostream>
+
+int main(int ac, char **av)
+{
+    if (ac == 4)
+    {
+        std::string line;
+        std::string file(av[1]); 
+        std::ofstream yourfile;
+        std::ifstream myfile;
+
+        yourfile.open(file + ".replace");
+        myfile.open(av[1]);
+        if (!myfile.good() || !yourfile.good())
+            return 1;
+        while (getline(myfile, line))
+        {
+            size_t pos = line.find(av[2]);
+            while (pos != std::string::npos)
+            {
+                line.erase(pos, std::string(av[2]).length());
+                line.insert(pos, av[3]);
+                pos = line.find(av[2]);
+            }
+            yourfile << line << std::endl;
+        }
+        myfile.close();
+        yourfile.close();
+    }
+}
